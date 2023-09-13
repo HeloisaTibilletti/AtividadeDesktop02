660c90f0ecc05c82fbb47fe6b619d43df95af284
Gui-leite233 <Nicole2005dembitzki@gmail.com>
2023-09-13 08:19:08 -0300

    Primeiro Commit

---
+++ chamaAPI.js
@@ -0,0 +1,32 @@
+function exibirPiada(){
+    axios.get('https://v2.jokeapi.dev/joke/Any').then(resposta => {
+        const piada = resposta.data;
+        exibirPiadaAleatoria(piada);
+    })
+}
+
+function exibirCachorro(){
+    axios.get('https://dog.ceo/api/breeds/image/random').then(resposta => {
+        const cachorro = resposta.data.message;
+        exibirImagemCachorro(cachorro);
+    })
+}
+
+const botao = document.getElementById('btChama');
+
+botao.addEventListener('click', () => {
+    exibirPiada();
+    exibirCachorro();
+})
+
+function exibirPiadaAleatoria(piada){
+    const pergunta = document.getElementById('tituloPiada');
+    const resposta = document.getElementById('respostaPiada');
+
+    pergunta.innerHTML = piada.setup;
+    resposta.innerHTML = piada.delivery;
+}
+
+function exibirImagemCachorro(cachorro){
+    const imagem = document.getElementById('cachorro').src = cachorro;
+}
